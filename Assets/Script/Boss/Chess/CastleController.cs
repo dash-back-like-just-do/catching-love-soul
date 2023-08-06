da346e1f31633fd2a9ae7a2429d1f6f9eb31e528
Dave <Dave@buho.game.tw>
2023-08-06 12:49:05 +0800

    add chess fade in

--- Assets/Script/Boss/Chess/CastleController.cs
+++ Assets/Script/Boss/Chess/CastleController.cs
@@ -1,21 +1,36 @@
+using System;
 using System.Collections;
+using DG.Tweening;
 using GameCore.Boss.ChessSpace;
 using UnityEngine;
 
 public class CastleController : Chess {
     [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] float speed;
     [SerializeField] float awakeTime;
-    
+    [SerializeField] float spawnTime;
 
     public override void OnSpawn(Vector2 diraction)
     {
-        StartCoroutine(waitForMoving());
+        SpawnAnim(()=>{
+            StartCoroutine(waitForMoving());
+        });
         IEnumerator waitForMoving(){
             yield return new WaitForSeconds(awakeTime);
             rigidbody2D.velocity = diraction*speed;
             yield return new WaitForSeconds(10);
             Destroy(gameObject);
         }
+    }
+    void SpawnAnim(Action onComplete)
+    {
+        Color color = spriteRenderer.color;
+        spriteRenderer.color = new Color(color.r, color.g, color.b, 0);
+        DOTween.ToAlpha(() => spriteRenderer.color, x => spriteRenderer.color = x, 1, spawnTime).OnComplete(() =>
+        {
+            onComplete();
+            
+        });
     }
 }
--- Assets/Script/Boss/Chess/PawnController.cs
+++ Assets/Script/Boss/Chess/PawnController.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using GameCore.Boss.ChessSpace;
 using UnityEngine;
 using DG.Tweening;
+using System;
+
 public class PawnController : Chess {
     [SerializeField] float jumpPower;
     [SerializeField] float duration;
     [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] float destoryTime;
     [SerializeField] float awakeTime;
-
+    [SerializeField] float spawnTime;
     public override void OnSpawn(Vector2 diraction)
     {
         GameObject player =  GameObject.FindWithTag("Player");
-        StartCoroutine(waitForMoving());
+        SpawnAnim(()=>{
+            StartCoroutine(waitForMoving());
+        });
         IEnumerator waitForMoving(){
             yield return new WaitForSeconds(awakeTime);
             transform.DOJump(player.transform.position,jumpPower,1,duration).OnComplete(OnComplete);
@@ -24,6 +28,16 @@ public class PawnController : Chess {
         });
     }
     
+    void SpawnAnim(Action onComplete)
+    {
+        Color color = spriteRenderer.color;
+        spriteRenderer.color = new Color(color.r, color.g, color.b, 0);
+        DOTween.ToAlpha(() => spriteRenderer.color, x => spriteRenderer.color = x, 1, spawnTime).OnComplete(() =>
+        {
+            onComplete();
+            
+        });
+    }
     public void TestSpawn(){
         OnSpawn(Vector2.zero);
     }
